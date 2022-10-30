9a6db074b230abd21488f247f0ff6f7fa96f074b
malkpoli <115538290+malkpoli@users.noreply.github.com>
2022-10-30 19:07:21 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -9,6 +9,22 @@ def get_count_char(str_):
             else:
                 dict_[value] = 1
     return dict_
+
+def percent_char(count_char_dict):
+    i = 0
+    percent_dict = {}
+    for char in count_char_dict:
+        i += count_char_dict.get(char) #общее количество букв
+    #print(i)
+    for char in count_char_dict:
+        percent_dict[char] = round((count_char_dict.get(char)/i)*100, 2)
+    #проверка, что сумма процентов равна 100
+    check = 0
+    for char in percent_dict:
+        check += percent_dict.get(char)
+    #print(round(check, 1))
+    return percent_dict
+
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
